f98bd8734b917c209d3c652822a50c79c472d8b0
JZanataNeto <cartasdojoao@gmail.com>
2025-01-08 10:33:33 -0300

    adicinando botao editar

--- js/api.js
+++ js/api.js
@@ -13,7 +13,7 @@ const api = {
     async salvarPensamento(pensamento) {
         try {
             const response = await fetch('http://localhost:3000/pensamentos', {
-                method: "Post",
+                method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
@@ -21,11 +21,39 @@ const api = {
             });
             return await response.json();
         } catch {
-            alert('Erro ao buscar pensamentos');
+            alert('Erro ao salvar pensamento');
             throw error;
             
         }
-    }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            return await response.json();
+        } catch {
+            alert('Erro ao buscar pensamento');
+            throw error;
+            
+        }
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        } catch {
+            alert('Erro ao editar pensamento');
+            throw error;
+            
+        }
+    },
 }
 
 export default api;
